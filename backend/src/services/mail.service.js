3024c65bed6f9699df35be5e2524d41ca7fd59c8
Raguls333 <raguls786@gmail.com>
2026-01-14 20:11:24 +0530

    modified titan to gmail smtp

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -11,7 +11,9 @@ let transporter = null;
 
 if (MAIL_ENABLED) {
  transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
